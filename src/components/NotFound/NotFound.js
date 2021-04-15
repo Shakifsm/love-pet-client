9a4508eb1514265e2a3fbd6fae0b2d5643fe9593
shakifsm <shakif20@gmail.com>
2021-04-16 00:39:45 +0600

    first commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{color:"red", textAlign:"center", paddingTop:"50px"}}>
+            <h1>Sorry Invalid Request !!</h1>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtj1SDVH2c6460eCOMRo02Ymz6q-E9QPs",
+    authDomain: "love-pet-81a10.firebaseapp.com",
+    projectId: "love-pet-81a10",
+    storageBucket: "love-pet-81a10.appspot.com",
+    messagingSenderId: "1069444699519",
+    appId: "1:1069444699519:web:9197a757773bfad054764a"
+  };
+
+  export default firebaseConfig;
